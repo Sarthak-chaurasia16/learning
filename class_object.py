a733b42b4f0befab4766c8cd20f409f27a31636c
Sarthak <sarthakchaurasia326@gmail.com>
2026-03-09 23:05:01 +0530

    Inheritance and Encapsulation

--- class_object.py
+++ class_object.py
@@ -28,5 +28,49 @@ for my_new_car in my_car:
 exit
 print("Exit Successfully:)")
 """
+#3. Inheritance 
+"""class Car:
+    def __init__(self ,brand , model):
+        self.brand = brand
+        self.model = model 
+    def full_name(self):
+        return f"{self.model} by {self .brand} "
+#Inherit kia hai isliye parenthesis ke andar parent class ka name likhna padega warna directly class name likh dete
+class ElectricCar(Car):
+    
+    #super() ka matlab hai derived class mein andar jo parent class ka name likha hai 
+    #use access krke uss class mein jao fir uske andar __init__ function access kro aur 
+    #usme brand aur model tha isliye iss wali mein brand and model likh denge 
+    
+    
+    def __init__(self,brand, model, battery_size):
+        super().__init__(brand , model)
+        self.battery_size = battery_size
+my_tesla = ElectricCar("Tesla", "Model S", "90KWh")
+print(my_tesla.full_name())
+exit
+print("Exit Successfully:)")"""
+#4. Encapsulation
+class Car:
+    #Attribute ke aage do underscore lagane se private ho jata hai like brand ke aage lagae hai
+    # Ab usko access krne ke liye get method banana padega kuki woh car class mein access ho jayega 
+    #but uska koi object bana kar access kroge toh access nhi hoga
+    def __init__(self ,brand , model):
+        self.__brand = brand
+        self.model = model 
+    def get_brand(self):
+        return self.full_name() + "! "
+    def full_name(self):
+        return f"{self.model} by {self .__brand} "
+
+class ElectricCar(Car):
+    def __init__(self,brand, model, battery_size):
+        super().__init__( brand , model)
+        self.battery_size = battery_size
+my_tesla = ElectricCar("Tesla", "Model S", "90KWh")
+#print(my_tesla.__brand)
+print(my_tesla.get_brand())
+exit
+print("Exit Successfully:)")
 
 
