93e03517636794750102fd7116724c264d3286e7
Sarthak <sarthakchaurasia326@gmail.com>
2026-03-29 00:23:01 +0530

    k

--- Array_attributes.py
+++ Array_attributes.py
@@ -33,8 +33,8 @@ a3 = np.arange(8).reshape(2,2,2)
 
 #___________________________________________________________________________________________________
 #Array Operation
-#a1 = np.arange(12).reshape(3,4)
-#a2 = np.arange(12,24).reshape(3,4)
+a1 = np.arange(12).reshape(3,4)
+a2 = np.arange(12,24).reshape(4,3)
 #Scalar Operation or Arithmatic operation
 #print(a1*3)
 #Relational(Check element condition and give boolean form answer)
@@ -45,11 +45,40 @@ a3 = np.arange(8).reshape(2,2,2)
 #--------------------------------------------------------------------------------------------------
 
 #____________________________Array Function ______________________________________________________________
-a1 = np.random.random((3,3))
-a1 = np.round(a1*100)
+#a1 = np.random.random((3,3))
+#a1 = np.round(a1*100)
 #print(a1)
 #max/min/sum/product
 #print(np.max(a1))
 #print(np.min(a1))
 #print(np.sum(a1))
 #print(np.prod(a1))
+#print(np.max(a1,axis=1))
+#0->Columns mein maximum (stand wali) And 1 -> Row (Means leti wali line mein max)
+
+#mean/median/std/var
+
+#print(np.mean(a1))
+#print(np.median(a1))
+#print(np.std(a1))
+#print(np.var(a1))
+
+#Trigonometry function
+#print(np.sin(a1))
+
+#Dot product means a1 ki column and a2 ka row ki value same honi chahiye like (3,4) & (4,6) tab hi ho payega agar nhi hoga toh dot product nhi hoga
+#print(np.dot(a1,a2))
+
+#Log and exponents
+#print(np.log(a1))
+#print(np.exp(a1))
+
+#round/floor/ceil
+#a1 =np.random.random((2,3))
+#print(a1)#Isse float mein aa raha hai
+#Round se nearest number ke paas le jayega float value
+#print(np.round(a1))# Isse int mein aa raha hai kuki round kar dia 
+#Floor peeche wale number ke paas le jayega float value
+#print(np.floor(a1))
+#Ceil se aage wali value aa jayegi
+#print(np.ceil(a1))
